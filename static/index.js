56f3bcb1deca7a3eade11606f1208246274e27e7
chenyanfei-m <1044927473@qq.com>
2018-07-18 18:54:53 +0800

    add hashHistory

--- static/index.js
+++ static/index.js
@@ -2,5 +2,7 @@ import routes from './route.js'
 import Router from './router.js'
 
 new Router({
+  // mode: 'hash',
+  mode: 'browser',
   route: routes
 })
---
+++ static/router.js
@@ -0,0 +1,44 @@
+import { NotFoundComponent } from './components.js'
+
+const getHash = () => {
+  const { hash } = window.location
+  if (!hash.length) window.location.hash = '#/'
+  return window.location.hash.replace(/^#\//, '')
+}
+
+const getPathname = (mode) => {
+  if (mode === 'browser') return location.pathname
+  if (mode === 'hash') return `/${getHash()}`
+}
+
+const genRender = (mode, route) => {
+  const findResult = route.find(ele => ele.path === getPathname(mode))
+  const routerView = document.querySelector('.router-view')
+  if (findResult) {
+    routerView.innerHTML = findResult.component
+  } else {
+    routerView.innerHTML = NotFoundComponent
+  }
+}
+
+export default function ({ mode, route }) {
+
+  const render = () => genRender(mode, route)
+
+  const push = (path, state) => {
+    if (mode === 'hash') window.location.hash = path
+    if (mode === 'browser') history.pushState(state, null, path)
+    window.$route.data = state
+    render()
+  }
+
+  window.$route = { push }
+  window.onload = () => render()
+  window.onpopstate = (e) => render()
+
+  //模拟router-link
+  const routerLinks = document.querySelectorAll('router-link')
+  routerLinks.forEach(ele => {
+    ele.onclick = (e) => push(e.target.getAttribute('path'))
+  })
+}
